5f332881efb9a692862889e6555854c3498f596c
Tumisang <mongwegeloa@gmail.com>
2023-08-25 00:40:39 -0700

    printf

--- main.h
+++ main.h
@@ -11,20 +11,60 @@
 
 #include <limits.h>
 
-#include <ctype.h>
+#define OUTPUT BUF_SIZE 1024
+#define BUF_FLUSH -1
 
-#define BUFFER_SIZE 1024
 
+#define NULL_STRING "(null)"
+
+#define PARAS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+#define CONVERT_LOWERCASE  1
+#define CONVERT_UNSIGNED   2
+
+/* _put.c */
+int _puts(char *str);
 int _putchar(char c);
+
+/* _printf.c */
 int _printf(const char *format, ...);
-void printf_buffer(char buffer[], int *buff_ind);
-int print_string(va_list args, int printed);
-int print_integer(va_list args, int printed);
+
+void print_buffer(char buffer[], int *buff_ind);
+/* number.c */
+char *convert(long int num, int base, int flags, int printed);
+int print_unsigned(va_list args, int printed);
+int print_address(va_list args, int printed);
+
+/* specifer.c */
+char *get_width(char *s, int printed, va_list args);
+int (*get_specifer(char *s))(va_list args, int printed);
+int get_print_func(char *s, va_list args, int printed);
+int get_flag(char *s, int printed);
+int get_modifier(char *s, int printed);
+
+/* print_number.c */
+int _isdigit(int c);
+int _strlen(char *s);
+int print_number_left_shift(char *str, int printed);
+int print_number(char *str, int printed);
+int print_number_right_shift(char *str, int printed);
+
+/* convert_number.c */
 int print_binary(unsigned int num, int printed);
 int print_hex(unsigned int num, int printed, int uppercase);
 int print_pointer(va_list args, int printed);
 int print_octal(unsigned int num, int printed, int uppercase);
-int print_char(va_list args, int printed);
 
+/* print_functions.c */
+int print_char(va_list args, int printed);
+int print_string(va_list args, int printed);
+int print_int(va_list args, int printed);
+int print_percent(va_list args, int printed);
+int print_S(va_list args, int printed);
+
+/* simple_pointers.c */
+int print_rev(va_list args, int printed);
+int print_from_to(char *start, char *stop, char *except);
+int print_rot13(va_list args, int printed);
 
 #endif
